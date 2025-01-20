f0ae55c9b523b8d9c978114b6d65655d65f43be4
AdmiralKeso <cas.hagenaar@gmail.com>
2025-01-20 14:25:14 +0100

    Add: Assign variables to menu items and mute sound btn

---
+++ script.js
@@ -0,0 +1,5 @@
+//Main menu
+var muteButton = document.getElementById('mute-btn');
+var audio = document.getElementById('background-music');
+var startgame = document.getElementById('start');
+var howToPlay = document.getElementById('htp');
