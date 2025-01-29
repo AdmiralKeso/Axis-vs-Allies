22aee3fbd3ac1f65fc5971725a466363ed22bfc3
AdmiralKeso <cas.hagenaar@gmail.com>
2025-01-29 11:28:55 +0100

    Add: Create a grid

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ var enemyGridSize = prompt('what size should you like your enemy grid to be?');
 var myGrid = createGrid(myGridSize);
 var enemyGrid = createGrid(enemyGridSize);
 
+function createGrid(size) { //Creating a grid using arrays. The arrays will have arrays itself meaning it will form a grid.
+  let grid = [];
+  for (let i = 0; i < size; i++) { //creates size number of rows.
+    grid[i] = [];
+    for (let j = 0; j < size; j++) {
+      grid[i][j] = '-'; //fills each row with '-'.
+    }
+  }
+  return grid;
+}
+
